6e0368ab5c7f99ff03e9aec94458d612baf87d35
Lev Khodzhaev <khodazhaev_la@etm.ru>
2025-05-11 15:35:20 +0300

    Add ListTasks in TaskFilter.cs

---
+++ TaskFilter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace TaskManager
+{
+    public class TaskFilter
+    {
+        public void ListTasks(List<Task> tasks, TaskStatus?  statusFilter = null, TaskPriority? priorityFilter = null)
+        {
+             
+        }
+    }
+}
