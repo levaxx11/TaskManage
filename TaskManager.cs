2d0e2f33a234fec95b5aa5b778da5b802f575f9e
Lev Khodzhaev <khodazhaev_la@etm.ru>
2025-05-12 10:34:37 +0300

    Add  new code to TaskManger.cs

--- TaskManager.cs
+++ TaskManager.cs
@@ -3,6 +3,38 @@ namespace TaskManager
     public class TaskManager
     {
         private readonly TaskRepository repository;
-          private readonly TaskFilter filter;
+        private readonly TaskFilter filter;
         private readonly TaskStorage storage;
+
+        public TaskManager()
+        {
+            repository = new TaskRepository();
+            filter = new TaskFilter();
+            storage = new TaskStorage();
+            repository.GetTasks().AddRange(storage.LoadTasks());
+        }
+
+        public void AddTask(string title, string description, TaskPriority priority)
+        {
+            repository.AddTask(title, description, priority);
+            storage.SaveTasks(repository.GetTasks());
+        }
+
+        public void EditTask(int id, string title, string description, TaskPriority priority, TaskStatus status)
+        {
+            repository.EditTask(id, title, description, priority, status);
+            storage.SaveTasks(repository.GetTasks());
+        }
+
+        public void DeleteTask(int id)
+        {
+            repository.DeleteTask(id);
+            storage.SaveTasks(repository.GetTasks());
+        }
+
+        public void ListTasks(TaskStatus? statusFilter = null, TaskPriority? priorityFilter = null)
+        {
+            filter.ListTasks(repository.GetTasks(), statusFilter, priorityFilter);
+        }
     }
+}
