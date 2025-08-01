fcb0684860f6fa5c711f267beadd4efbd1267998
levaxx11 <levaxx11@gmail.com>
2025-08-01 13:24:59 +0300

    Add DB

---
+++ TaskManageDB/TaskManageDB/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TaskManagerDB
+{
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString =  "Data Source=LEVAXX11\\MSSQLSERVER2022;Initial Catalog=TaskManagerDB;User ID=levax; Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection to the database was successful.");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error connecting to the database: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection closed.");
+                }
+            }   
+        }
+    }
+}
